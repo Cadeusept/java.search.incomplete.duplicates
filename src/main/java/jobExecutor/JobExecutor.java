a566212e8cdfb6a3a51f29feb1267b176bf48df6
cadeusept <matwey.likhota@gmail.com>
2024-03-17 11:46:46 +0300

    feat: added job executor and main draft

--- src/main/java/jobExecutor/JobExecutor.java
+++ src/main/java/jobExecutor/JobExecutor.java
@@ -1,4 +1,74 @@
 package jobExecutor;
 
-public class JobExecutor {
+import java.util.Queue;
+import java.util.concurrent.*;
+import java.util.Vector;
+
+public class JobExecutor implements Executor {
+    private final Queue<Runnable> jobQueue = new ConcurrentLinkedQueue<>();
+    private volatile boolean isRunning = true;
+    private Vector<Thread> threads = null;
+    private int threadsCounter = 0;
+
+    public final boolean IsShutdown() {
+        return !isRunning;
+    }
+
+    public final Vector<Thread> GetThreads() {
+        return threads;
+    }
+
+    public final int GetThreadsCounter() {
+        return threadsCounter;
+    }
+
+    private final class Job implements Runnable {
+
+        @Override
+        public void run() {
+            while (isRunning) {
+                Runnable nextJob = jobQueue.poll();
+                if (nextJob != null) {
+                    nextJob.run();
+                }
+            }
+        }
+    }
+
+    public JobExecutor(int threadsCounterInitValue) {
+        threads = new Vector<Thread>();
+        for (int i = 0; i < threadsCounterInitValue; i++) {
+            threads.add(new Thread(new Job()));
+            threads.get(i).start();
+        }
+
+        threadsCounter = threadsCounterInitValue;
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        if (isRunning) {
+            jobQueue.offer(command);
+        }
+    }
+
+    public void executeTasks(Queue<Runnable> tasks) {
+        for (int i = 0; i < tasks.size(); i++) {
+            execute(tasks.poll());
+        }
+    }
+
+    public void shutdown() {
+        isRunning = false;
+    }
+
+    public void joinThread(int threadIndex) throws InterruptedException {
+        threads.get(threadIndex).join();
+    }
+
+    public void joinAllThreads() throws InterruptedException {
+        for (int i=0; i<threadsCounter; ++i) {
+            joinThread(i);
+        }
+    }
 }
--- src/main/java/parser/Main.java
+++ src/main/java/parser/Main.java
@@ -1,7 +1,34 @@
 package parser;
 
+import java.io.IOException;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import jobExecutor.JobExecutor;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws ExecutionException, InterruptedException  {
+        System.out.println("Setting up environment");
+        JobExecutor threadPool = new JobExecutor(2);
+        Queue<Runnable> tasks = new ConcurrentLinkedQueue<Runnable>();
+
+        Runnable runRMQ = () -> {
+            try {
+                RabbitProducer.runProducer();
+            } catch (IOException | TimeoutException e) {
+                e.printStackTrace();
+            }
+        };
+
+        tasks.add(runRMQ);
+        tasks.add(runRMQ);
+
+        threadPool.executeTasks(tasks);
+
+        threadPool.shutdown();
+
+        threadPool.joinAllThreads();
     }
 }
